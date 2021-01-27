8a5bfd9c7e5f62f9cae7be769a1211b7a91df494
Fernando Augusto <f3rnand0_lp@hotmail.com>
2021-01-27 15:02:02 -0400

    Perguntas adicionadas

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 // src/components/Footer/index.js
 const FooterWrapper = styled.footer`
-  background-color: #00000070;
+  background-color: #000000AB;
   padding: 20px;
   display: flex;
   align-items: center;
