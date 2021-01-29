f5ff383761aaf43e0b51c09159cce6921a0bda5f
Fernando Augusto <f3rnand0_lp@hotmail.com>
2021-01-29 15:05:05 -0400

    Header

--- pages/_app.js
+++ pages/_app.js
@@ -34,6 +34,8 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>Quiz Design Patterns</title>
+        <meta name="description" content="Teste os seus conhecimentos sobre Design Patterns e comece a usá-los, além do Singleton" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
